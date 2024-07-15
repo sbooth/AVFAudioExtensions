9cda5651797b859c20b4c72902a4250d6338858c
Stephen F. Booth <me@sbooth.org>
2024-07-15 09:39:03 -0500

    Update formatting

--- Package.swift
+++ Package.swift
@@ -6,19 +6,22 @@ import PackageDescription
 let package = Package(
 	name: "AVFAudioExtensions",
 	products: [
-		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
 			name: "AVFAudioExtensions",
-			targets: ["AVFAudioExtensions"]),
+			targets: [
+				"AVFAudioExtensions",
+			]),
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
 			name: "AVFAudioExtensions",
-			linkerSettings: [.linkedFramework("AVFAudio")]),
+			linkerSettings: [
+				.linkedFramework("AVFAudio"),
+			]),
 		.testTarget(
 			name: "AVFAudioExtensionsTests",
-			dependencies: ["AVFAudioExtensions"]),
+			dependencies: [
+				"AVFAudioExtensions",
+			]),
 	]
 )
