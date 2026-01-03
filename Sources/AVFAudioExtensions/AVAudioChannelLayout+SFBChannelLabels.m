9b9b3c479ff943ba79292d508f6fa1463cb6b9ce
Stephen Booth <me@sbooth.org>
2026-01-02 18:19:36 -0600

    Add SPDX tags (#30)

--- Sources/AVFAudioExtensions/AVAudioChannelLayout+SFBChannelLabels.m
+++ Sources/AVFAudioExtensions/AVAudioChannelLayout+SFBChannelLabels.m
@@ -1,7 +1,8 @@
 //
-// Copyright © 2013-2024 Stephen F. Booth <me@sbooth.org>
+// SPDX-FileCopyrightText: 2013 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/AVFAudioExtensions
-// MIT license
 //
 
 #import <stdlib.h>
