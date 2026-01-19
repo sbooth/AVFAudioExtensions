604ff062731307377731a1769b907b793f6c5513
Stephen Booth <me@sbooth.org>
2026-01-19 10:59:36 -0600

    Updated

--- Sources/AVFAudioExtensions/AVAudioChannelLayout+SFBLayoutEquivalence.m
+++ Sources/AVFAudioExtensions/AVAudioChannelLayout+SFBLayoutEquivalence.m
@@ -1,12 +1,13 @@
 //
-// Copyright © 2024 Stephen F. Booth <me@sbooth.org>
+// SPDX-FileCopyrightText: 2024 Stephen F. Booth <contact@sbooth.dev>
+// SPDX-License-Identifier: MIT
+//
 // Part of https://github.com/sbooth/AVFAudioExtensions
-// MIT license
 //
 
-@import AudioToolbox.AudioFormat;
-
 #import "AVAudioChannelLayout+SFBLayoutEquivalence.h"
+
+@import AudioToolbox.AudioFormat;
 
 @implementation AVAudioChannelLayout (SFBLayoutEquivalence)
 
